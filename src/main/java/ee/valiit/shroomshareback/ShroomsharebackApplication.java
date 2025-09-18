c4debffd43b6af45ea0fd985c956c3762dad7f84
Kristiina <kristiina@gmail.com>
2025-09-18 10:37:15 +0300

    initial commit

---
+++ src/main/java/ee/valiit/shroomshareback/ShroomsharebackApplication.java
@@ -0,0 +1,13 @@
+package ee.valiit.shroomshareback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShroomsharebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShroomsharebackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/valiit/shroomshareback/ShroomsharebackApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.valiit.shroomshareback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShroomsharebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
