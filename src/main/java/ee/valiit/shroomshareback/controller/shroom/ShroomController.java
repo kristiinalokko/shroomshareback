13e80d867955c1c72dc84c4a621f847b709a1eba
InnarM <innar123@gmail.com>
2025-09-25 13:30:54 +0300

    wip

--- src/main/java/ee/valiit/shroomshareback/controller/shroom/ShroomController.java
+++ src/main/java/ee/valiit/shroomshareback/controller/shroom/ShroomController.java
@@ -2,12 +2,11 @@ package ee.valiit.shroomshareback.controller.shroom;
 
 import ee.valiit.shroomshareback.controller.shroom.dto.ShroomBasicInfo;
 import ee.valiit.shroomshareback.controller.shroom.dto.ShroomInfo;
+import ee.valiit.shroomshareback.controller.shroom.dto.ShroomProfile;
 import ee.valiit.shroomshareback.service.ShroomService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,6 +15,12 @@ import java.util.List;
 public class ShroomController {
 
     private final ShroomService shroomService;
+
+    @PostMapping("/shroom")
+    @Operation(summary = "Add shroom")
+    public void addShroom(@RequestBody ShroomProfile shroomProfile){
+        shroomService.addShroom(shroomProfile);
+    }
 
     @GetMapping("/shroom")
     @Operation(summary = "Get shroom info")
