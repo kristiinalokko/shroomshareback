01f029f0d93d33bd89f34889d24bda5ce853c2cc
Kristiina <kristiina@gmail.com>
2025-10-01 11:59:36 +0300

    locationimagecontroller tehtud

--- src/main/java/ee/valiit/shroomshareback/service/LocationService.java
+++ src/main/java/ee/valiit/shroomshareback/service/LocationService.java
@@ -16,6 +16,7 @@ import ee.valiit.shroomshareback.persistence.user.User;
 import ee.valiit.shroomshareback.persistence.user.UserRepository;
 import ee.valiit.shroomshareback.util.BytesConverter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
